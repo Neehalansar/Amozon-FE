6b4b4aa3b6d6bc55e6ecef6180623b22b0f63d5a
Neehalansar <147186095+Neehalansar@users.noreply.github.com>
2024-01-12 20:43:26 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-ci-cd-pipeline"  # Replace with your actual S3 bucket name
+    bucket = "project-bucke"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "eu-west-3" 
   }
